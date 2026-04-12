d45698cada93f9094fcd39a5012112c1e7016ba9
whiteC0br4 <white.c0br4@gmail.com>
2026-04-12 15:25:51 +0200

    finish webTech

--- server/server.js
+++ server/server.js
@@ -1,102 +1,56 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 
 // Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, 'files')));
-
-// Configure a 'get' endpoint for data..
-//app.get('/movies', function (req, res) {
-  // Part 1: Remove the next line and replace with your code
-  //res.send('!dlrow olleH')
-//})
 
 app.get('/movies', function (req, res) {
   const movies = [
     {
       Title: "The Shawshank Redemption",
-      Year: "1994",
-      Rated: "R",
-      Released: "14 Oct 1994",
-      Runtime: "142 min",
-      Genre: "Drama",
-      Director: "Frank Darabont",
-      Writer: "Stephen King, Frank Darabont",
-      Actors: "Tim Robbins, Morgan Freeman, Bob Gunton",
+      Released: "1994-10-14",
+      Runtime: 142,
+      Genres: ["Drama"],
+      Directors: ["Frank Darabont"],
+      Writers: ["Stephen King", "Frank Darabont"],
+      Actors: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"],
       Plot: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-      Language: "English",
-      Country: "United States",
-      Awards: "Nominated for 7 Oscars. 20 wins & 31 nominations total",
       Poster: "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_SX300.jpg",
-      Metascore: "80",
-      imdbRating: "9.3",
-      imdbVotes: "2,500,000",
-      imdbID: "tt0111161",
-      Type: "movie",
-      DVD: "N/A",
-      BoxOffice: "$28,341,469",
-      Production: "Columbia Pictures",
-      Website: "N/A",
-      Response: "True"
+      Metascore: 80,
+      imdbRating: 9.3
     },
     {
       Title: "The Godfather",
-      Year: "1972",
-      Rated: "R",
-      Released: "24 Mar 1972",
-      Runtime: "175 min",
-      Genre: "Crime, Drama",
-      Director: "Francis Ford Coppola",
-      Writer: "Mario Puzo, Francis Ford Coppola",
-      Actors: "Marlon Brando, Al Pacino, James Caan",
+      Released: "1972-03-24",
+      Runtime: 175,
+      Genres: ["Crime", "Drama"],
+      Directors: ["Francis Ford Coppola"],
+      Writers: ["Mario Puzo", "Francis Ford Coppola"],
+      Actors: ["Marlon Brando", "Al Pacino", "James Caan"],
       Plot: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-      Language: "English, Italian, Latin",
-      Country: "United States",
-      Awards: "Won 3 Oscars. 30 wins & 30 nominations total",
       Poster: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-      Metascore: "100",
-      imdbRating: "9.2",
-      imdbVotes: "1,700,000",
-      imdbID: "tt0068646",
-      Type: "movie",
-      DVD: "N/A",
-      BoxOffice: "$134,966,411",
-      Production: "Paramount Pictures",
-      Website: "N/A",
-      Response: "True"
+      Metascore: 100,
+      imdbRating: 9.2
     },
     {
       Title: "The Dark Knight",
-      Year: "2008",
-      Rated: "PG-13",
-      Released: "18 Jul 2008",
-      Runtime: "152 min",
-      Genre: "Action, Crime, Drama",
-      Director: "Christopher Nolan",
-      Writer: "Jonathan Nolan, Christopher Nolan, David S. Goyer",
-      Actors: "Christian Bale, Heath Ledger, Aaron Eckhart",
+      Released: "2008-07-18",
+      Runtime: 152,
+      Genres: ["Action", "Crime", "Drama"],
+      Directors: ["Christopher Nolan"],
+      Writers: ["Jonathan Nolan", "Christopher Nolan", "David S. Goyer"],
+      Actors: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
       Plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-      Language: "English, Mandarin",
-      Country: "United States, United Kingdom",
-      Awards: "Won 2 Oscars. 159 wins & 161 nominations total",
       Poster: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
-      Metascore: "84",
-      imdbRating: "9.0",
-      imdbVotes: "2,400,000",
-      imdbID: "tt0468569",
-      Type: "movie",
-      DVD: "N/A",
-      BoxOffice: "$534,858,444",
-      Production: "Warner Bros. Pictures",
-      Website: "N/A",
-      Response: "True"
+      Metascore: 84,
+      imdbRating: 9.0
     }
   ];
 
   res.json(movies);
 });
 
-app.listen(3000)
-
-console.log("Server now listening on http://localhost:3000/")
-
+app.listen(3000, () => {
+  console.log("Server now listening on http://localhost:3000/");
+});
